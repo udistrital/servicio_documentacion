20127a983f6f8476dd5eae4ad37fe14118f4f00e
Fabio Parra <faparraf@gmail.com>
2017-08-12 22:54:59 -0500

    Modularización y consumo de proyectos inscritos

--- controllers/documento_pago.py
+++ controllers/documento_pago.py
@@ -3,16 +3,31 @@ import requests
 import pprint
 
 def generar_documento_pago(parametros):
-
-    request_headers = {"Content-type" : "application/json"}
-    request_data = {"username" : parametros["username"], "password" : parametros["password"]}
-    request_url = "https://tuleap.udistrital.edu.co/api/tokens"
-    request = requests.post(url=request_url, json=request_data, headers=request_headers)
-    user_data = request.json()
-    pprint.pprint(user_data)
-
+    parametros["url_base"]="https://tuleap.udistrital.edu.co/api/"
+    user_data = autenticar_tuleap(parametros)
+    parametros["user_data"] = user_data
+    membership = get_membership(parametros)
     # user_id = response+["user_id"]
     # token   = response["token"]
     # uri     = response["uri"]
 
     return None
+
+def autenticar_tuleap(parametros):
+    request_headers = {"Content-type" : "application/json"}
+    request_data = {"username" : parametros["username"], "password" : parametros["password"]}
+    request_url = parametros["url_base"]+"tokens"
+    request = requests.post(url=request_url, json=request_data, headers=request_headers)
+    user_data = request.json()
+    pprint.pprint(user_data)
+    return user_data
+
+def get_membership(parametros):
+    request_headers = {"Content-type" : "application/json",
+    "X-Auth-Token":str(parametros["user_data"]["token"]),
+    "X-Auth-UserId":str(parametros["user_data"]["user_id"])}
+    request_url = parametros["url_base"]+"users/"+str(parametros["user_data"]["user_id"])+"/membership"
+    request = requests.get(url=request_url, headers=request_headers)
+    membership = request.json()
+    pprint.pprint(membership)
+    return membership
