8a2ac348420e011b28fe4a0c1dade8614d2486af
faparraf <faparraf@gmail.com>
2017-08-31 11:41:15 -0500

    Consumo de servicios para extracción de artefactos y filtrado por query

--- controllers/documento_pago.py
+++ controllers/documento_pago.py
@@ -3,7 +3,7 @@ import pprint
 import tuleap_api
 
 def generar_documento_pago(parametros):
-    parametros["url_base"]="https://tuleap.udistrital.edu.co/api/"
+    parametros["url_base_tuleap"]="https://tuleap.udistrital.edu.co/api/"
     project_string_key = "_project"
     user_data = tuleap_api.autenticar_tuleap(parametros)
     parametros["user_data"] = user_data
@@ -18,7 +18,15 @@ def generar_documento_pago(parametros):
             project_info = tuleap_api.get_project_info_tuleap(parametros, project_shortname)
             if len(project_info) > 0:
                 project_member_info.append(project_info[0])
-    # pprint.pprint(project_member_info)
+        # pprint.pprint(project_member_info)
+    contador_proyectos = 0
     for project in project_member_info:
-        pprint.pprint(tuleap_api.get_trackers_tuleap(parametros, project["id"]))
+        trackers = tuleap_api.get_trackers_tuleap(parametros, project["id"])
+        pprint.pprint("Escaneando " + str(project['shortname']))
+        for tracker in trackers:
+            #pprint.pprint(str(tracker['id']) + str(tracker['item_name']))
+            artifacts = tuleap_api.get_tracker_artifacts(parametros, str(tracker['id']))
+            pprint.pprint(artifacts)
+        contador_proyectos = contador_proyectos + 1
+        print("Porcentaje de proyectos escaneados " + str(contador_proyectos*100/len(project_member_info)) + "%")
     return None
--- controllers/tuleap_api.py
+++ controllers/tuleap_api.py
@@ -32,6 +32,40 @@ def get_trackers_tuleap(parametros, project_id):
     # pprint.pprint(project_trackers)
     return project_trackers
 
+def get_tracker_info_tuleap(parametros, tracker_id):
+    request_headers = {"Content-type" : "application/json",
+    "X-Auth-Token":str(parametros["user_data"]["token"]),
+    "X-Auth-UserId":str(parametros["user_data"]["user_id"])}
+    request_url = parametros["url_base_tuleap"]+"trackers/"+str(tracker_id)+""
+    # requests_params = {"shortname":project_shortname}
+    request = requests.get(url=request_url, headers=request_headers)
+    tracker_info = request.json()
+    # pprint.pprint(tracker_info)
+    return tracker_info
+
+def get_tracker_artifacts(parametros, tracker_id):
+    request_headers = {"Content-type" : "application/json",
+    "X-Auth-Token":str(parametros["user_data"]["token"]),
+    "X-Auth-UserId":str(parametros["user_data"]["user_id"])}
+    request_url = parametros["url_base_tuleap"]+"trackers/"+str(tracker_id)+"/artifacts"
+    requests_params = {"query":json.dumps(parametros["query"])}
+    #requests_params = {"query":json.dumps({"artifact_id":"14919"})}
+    #requests_params = {"query":build_query_object(parametros["query"], tracker_id)}
+    request = requests.get(url=request_url, headers=request_headers, params=requests_params)
+    print("Peticion " + request.url)
+    tracker_artifacts = request.json()
+    # pprint.pprint(tracker_artifacts)
+    return tracker_artifacts
+
+def build_query_object(query, tracker_id):
+    tracker_info = get_tracker_info_tuleap(parametros, str(tracker_id))
+    query_object = {}
+    for field in tracker_info["fields"]:
+        if field["name"] in query:
+            query_object[field["field_id"]] = query[field["name"]]
+    # pprint.pprint(tracker_artifacts)
+    return query_object
+
 def get_project_info_tuleap(parametros, project_shortname):
     request_headers = {"Content-type" : "application/json",
     "X-Auth-Token":str(parametros["user_data"]["token"]),
