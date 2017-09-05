98cc83666ad9985782ab8248bdf2932e7125da12
Fabio Parra <faparraf@gmail.com>
2017-09-05 15:22:03 -0500

    Modificación de función de comparación para recibir arreglos con regla lógica OR

--- controllers/utils.py
+++ controllers/utils.py
@@ -5,28 +5,33 @@ def filter_json_array(json_array, query):
 	#pprint.pprint(json_array)
 	for key, value in query.items():
 		data = None
-		# #print("Llave: "+str(key)+"     Valor: "+str(value))
-		# data[key]=[]
-		# for obj in json_array:
-		# 	#pprint.pprint(obj[key])
-		# 	if globals()[str(value["operator"])](obj[key], value["value"]):
-		# 		#pprint.pprint(obj)
-		# 		data[key].append(obj)
 		data = [obj for obj in json_array if(globals()[str(value["operator"])](obj[key], value["value"]))]
 		json_array = data
 	return json_array
 
 def equal(param1, param2):
 	#print (str(param1)+"="+str(param2))
-	try:
-		object_target = iso8601.parse_date(param1)
-		object_compare = iso8601.parse_date(param2)
-	except:
-		object_target = str(param1)
-		object_compare = str(param2)
+	if len(param2)>0:
+		for key in param2:
+			try:
+				object_target = iso8601.parse_date(param1)
+				object_compare = iso8601.parse_date(key)
+			except:
+				object_target = str(param1)
+				object_compare = str(key)
 
-	if object_target == object_compare:
-		return True
+			if object_target == object_compare:
+				return True
+	else:
+		try:
+			object_target = iso8601.parse_date(param1)
+			object_compare = iso8601.parse_date(param2)
+		except:
+			object_target = str(param1)
+			object_compare = str(param2)
+
+		if object_target == object_compare:
+			return True
 	return False
 
 def between(param1, param2):
@@ -75,3 +80,4 @@ def g_than(param1, param2):
 	if object_target > object_compare:
 		return True
 	return False
+	
