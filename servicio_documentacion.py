39ca18edbf97482f3a1d31de9ba653cea36da8d2
Fabio Parra <faparraf@gmail.com>
2017-09-07 03:42:07 -0500

    Revisión de plantilla de informe de gestión

--- servicio_documentacion.py
+++ servicio_documentacion.py
@@ -21,23 +21,23 @@ def generar_cumplido():
 	usuario_data["dia_final"]="30"
 	usuario_data["mes"]="Agosto"
 
-	# usuario_data["documento"]={}
-	# usuario_data["documento"]["tipo"]="Cedula de Ciudadania"
-	# usuario_data["documento"]["numero"]="1030577784"
-	# usuario_data["documento"]["ciudad"]="Bogota"
+	usuario_data["documento"]={}
+	usuario_data["documento"]["tipo"]="Cedula de Ciudadania"
+	usuario_data["documento"]["numero"]="1030577784"
+	usuario_data["documento"]["ciudad"]="Bogota"
 
-	# usuario_data["valor_mensual"]={}
-	# usuario_data["valor_mensual"]["formato_letras"]="Valor en letras"
-	# usuario_data["valor_mensual"]["formato_moneda"]="3579000"
+	usuario_data["valor_mensual"]={}
+	usuario_data["valor_mensual"]["formato_letras"]="Valor en letras"
+	usuario_data["valor_mensual"]["formato_moneda"]="3579000"
 	
-	# usuario_data["cuenta"]={}
-	# usuario_data["cuenta"]["tipo"]="Ahorros"
-	# usuario_data["cuenta"]["numero"]="692-111222-87"
-	# usuario_data["cuenta"]["banco"]="Bancolombia"
+	usuario_data["cuenta"]={}
+	usuario_data["cuenta"]["tipo"]="Ahorros"
+	usuario_data["cuenta"]["numero"]="692-111222-87"
+	usuario_data["cuenta"]["banco"]="Bancolombia"
 
-	# jefe_data={}
-	# jefe_data["nombre_completo"]="Beatriz Algo"
-	#jefe_data["cargo"]="Jefe Asesora de Sistemas"
+	jefe_data={}
+	jefe_data["nombre_completo"]="Beatriz Algo"
+	jefe_data["cargo"]="Jefe Asesora de Sistemas"
 
 	try:
 		return render_template('cumplido/template_cumplido.html', usuario=usuario_data, jefe=jefe_data)
