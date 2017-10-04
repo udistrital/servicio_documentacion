85620d784eeeedf927dc2b0baf014b2fa1c5a3f1
Fabio Parra <faparraf@gmail.com>
2017-10-04 15:15:18 -0500

    #16662# Se corrigen las diferencias necesarias para python 3 en los controladores y servicio publicado, se elimina codificación

--- controllers/comment_tuleap_from_github.py
+++ controllers/comment_tuleap_from_github.py
@@ -1,6 +1,6 @@
 
-import tuleap_api
-import github_api
+from controllers import tuleap_api
+from controllers import github_api
 # from github import Github
 import pprint
 import re
@@ -21,13 +21,13 @@ def comment_tuleap_from_github_func(parametros):
                 task_regex_group = re.search('##(.+?) ', commit_info["commit"]["message"])
                 if task_regex_group:
                     artifact_id = task_regex_group.group(1)
-                    print "Artifact # --" + str(artifact_id)+"--"
-                    print "Status "+str(tuleap_api.send_comment_tuleap(parametros, str(commit_info["commit"]["message"]), str(artifact_id)))
+                    print("Artifact # --" + str(artifact_id)+"--")
+                    print("Status "+str(tuleap_api.send_comment_tuleap(parametros, str(commit_info["commit"]["message"]), str(artifact_id))))
                 else:
-                    print "No hay tracker asociado"
+                    print("No hay tracker asociado")
                 # pprint.pprint(commit_info["commit"]["message"])
             else:
-                print "Not Found"
+                print("Not Found")
         # break
     # user_data = tuleap_api..autenticar_tuleap(parametros)
     # parametros["user_data"] = user_data
--- controllers/text_tool.py
+++ controllers/text_tool.py
@@ -1,8 +1,18 @@
 import re
 
-def format_comment_by_expr(text, expresion, replace):
+def format_comment_by_expr(text, expresion, replace, commitInfo):
     task_regex_group = re.search(expresion, text)
-    print("text")
-    print(task_regex_group.group(1))
-    print(re.sub(expresion, '', text))
-    return {'message':re.sub(expresion, '', text), 'artifact_id':task_regex_group.group(1)}
+    comment = re.sub(expresion, '', text)
+    comment = comment + '\n'
+    comment = comment + '****************************************************************\n'
+    comment = comment + '****************************************************************\n'
+    comment = comment + 'Git Commiter Info: '+'\n' 
+    comment = comment +"Autor: " +commitInfo['author']['name']+'\n' 
+    comment = comment + "E-mail: "+commitInfo['author']['email']+'\n' 
+    if 'username' in commitInfo['author']:
+        comment = comment + "Username: "+commitInfo['author']['username']+'\n' 
+    comment = comment + '****************************************************************\n'
+    comment = comment + '****************************************************************\n'
+    comment = comment + 'Commit Info:'+'\n' 
+    comment = comment + '--> '+commitInfo['url']
+    return {'message':comment, 'artifact_id':task_regex_group.group(1)}
